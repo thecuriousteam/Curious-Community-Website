ab441d810db634c7d4fbb5e460824be8a6759aca
thecuriousteam <curiousdevelopers.in@gmail.com>
2024-10-01 12:30:49 +0530

    Initial Project Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="font-mono text-pretty text-[20px] font-bold">
+				Curious Developers Community
+			</div>
+		</>
+	);
+}
+
+export default App;
